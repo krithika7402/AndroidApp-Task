adf45f10374565377b53a8d22c47d634391e5b52
krithika7402 <krithikan@presidio.com>
2023-10-02 10:31:38 +0530

    Minor updates in Layout

--- app/src/main/java/com/example/androidappweek1/MainActivity.kt
+++ app/src/main/java/com/example/androidappweek1/MainActivity.kt
@@ -10,12 +10,14 @@ import androidx.annotation.Dimension
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
@@ -24,7 +26,9 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -44,6 +48,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 
 import androidx.compose.ui.text.input.TextFieldValue
@@ -79,17 +84,15 @@ class MainActivity : ComponentActivity() {
                     startDestination = "login"
                 ) {
                     composable("login") { LoginScreen(navController) }
-                    composable("details/{username}/{password}",
+                    composable("details/{username}",
                         arguments = listOf(
                             navArgument("username") { type = NavType.StringType },
-                            navArgument("password") { type = NavType.StringType }
                         )
                     ) { backStackEntry ->
                         val username =
                             backStackEntry.arguments?.getString("username") ?: "No Username"
-                        val password =
-                            backStackEntry.arguments?.getString("password") ?: "No Password"
-                        DetailsScreen(username, password)
+
+                        DetailsScreen(username)
                     }
                 }
             }
@@ -108,77 +111,72 @@ fun MyTheme(content: @Composable () -> Unit) {
 @Composable
 fun LoginScreen(navController: NavHostController) {
     var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
 
-    ConstraintLayout(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
     ) {
-        val (logo, usernameTextField, passwordTextField, loginButton) = createRefs()
-
-        // Logo
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .constrainAs(logo) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(usernameTextField.top, margin = 16.dp)
-                }
-                .fillMaxWidth()
-                .height(100.dp)
-        )
-
-        // Username Text Field
-        TextField(
-            value = username,
-            onValueChange = { username = it },
-            singleLine = true,
-            modifier = Modifier
-                .constrainAs(usernameTextField) {
-                    top.linkTo(logo.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(passwordTextField.top, margin = 8.dp)
-                }
-                .fillMaxWidth()
-        )
-
-        // Password Text Field
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            singleLine = true,
-            modifier = Modifier
-                .constrainAs(passwordTextField) {
-                    top.linkTo(usernameTextField.bottom, margin = 8.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(loginButton.top, margin = 16.dp)
-                }
-                .fillMaxWidth()
-        )
-
-        // Login Button
-        Button(
-            onClick = {
-                // Pass the username and password to the next screen
-                navController.navigate("details/$username/$password")
-            },
-            modifier = Modifier
-                .constrainAs(loginButton) {
-                    top.linkTo(passwordTextField.bottom, margin = 16.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                }
-                .fillMaxWidth()
+        ConstraintLayout(
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Login")
+            val (logo, usernameTextField, spacer, loginButton) = createRefs()
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .constrainAs(logo) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(usernameTextField.top, margin = 16.dp)
+                    }
+                    .fillMaxWidth()
+                    .height(100.dp)
+            )
+
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                singleLine = true,
+                modifier = Modifier
+                    .constrainAs(usernameTextField) {
+                        top.linkTo(logo.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                    .fillMaxWidth()
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .constrainAs(spacer) {
+                        top.linkTo(usernameTextField.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(loginButton.top, margin = 16.dp)
+                    }
+                    .fillMaxWidth()
+                    .height(16.dp)
+            )
+
+            Button(
+                onClick = {
+                    navController.navigate("details/$username")
+                },
+                modifier = Modifier
+                    .constrainAs(loginButton) {
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Submit")
+            }
         }
     }
 }
@@ -188,29 +186,26 @@ interface JsonPlaceholderApi {
     suspend fun getPosts(): List<Post>
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun DetailsScreen(username: String, password: String) {
+fun DetailsScreen(username: String) {
     var posts by remember { mutableStateOf<List<Post>>(emptyList()) }
     var isLoading by remember { mutableStateOf(true) }
 
-    // Create a Retrofit instance
     val retrofit = Retrofit.Builder()
         .baseUrl("https://jsonplaceholder.typicode.com/")
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
-    // Create an instance of the JsonPlaceholderApi
     val api = retrofit.create(JsonPlaceholderApi::class.java)
 
-    // Make the API call when the composable is first displayed
     LaunchedEffect(username) {
         try {
             val response = api.getPosts()
             posts = response
             isLoading = false
         } catch (e: Exception) {
-            // Handle the error
             isLoading = false
         }
     }
@@ -218,7 +213,7 @@ fun DetailsScreen(username: String, password: String) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Posts") }
+                title = { Text(text = "Welcome, $username!") }
             )
         },
         content = {
@@ -226,68 +221,58 @@ fun DetailsScreen(username: String, password: String) {
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
             ) {
-                if (isLoading) {
-                    // Display a loading indicator while making the API call
-                    CircularProgressIndicator()
-                } else if (posts.isNotEmpty()) {
-                    // Display the list of posts once the API call is complete
-                    Text(
-                        text = "Welcome, $username!",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                    Text(
-                        text = "Your password: $password",
-                        fontSize = 18.sp,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
+                Spacer(modifier = Modifier.height(56.dp))
 
-                    // Display the list of posts in a scrollable LazyColumn
-                    LazyColumn {
+                if (isLoading) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                } else if (posts.isNotEmpty()) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
                         items(posts) { post ->
-                            Column(
-                                modifier = Modifier.padding(bottom = 16.dp),
-                                horizontalAlignment = Alignment.CenterHorizontally
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
                             ) {
-                                Text(
-                                    text = "Title: ${post.title}",
-                                    fontSize = 18.sp,
-                                    textAlign = TextAlign.Center
-                                )
-                                Spacer(modifier = Modifier.height(8.dp))
-                                Text(
-                                    text = "Body: ${post.body}",
-                                    fontSize = 18.sp,
-                                    textAlign = TextAlign.Center
-                                )
+                                Column(
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                ) {
+                                    Text(
+                                        text = "#${post.id} ${post.title}",
+                                        fontSize = 18.sp,
+                                        fontWeight = FontWeight(700)
+                                    )
+                                    Spacer(modifier = Modifier.height(8.dp))
+                                    Text(
+                                        text = "${post.body}",
+                                        fontSize = 12.sp,
+                                    )
+                                }
                             }
                         }
                     }
 
-                    Button(
-                        onClick = {
-                            // Implement any action you want here when the button is clicked.
-                        },
-                        modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
-                    ) {
-                        Text(text = "Logout")
-                    }
                 } else {
-                    // Handle the case where the API call fails or there are no posts
                     Text(
                         text = "Failed to fetch data or no posts available",
-                        fontSize = 18.sp
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.CenterHorizontally)
                     )
                 }
             }
         }
     )
 }
-
 
 @Preview(showBackground = true)
 @Composable
